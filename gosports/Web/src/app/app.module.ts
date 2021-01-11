d15944734d963748c6cd06a350c6b54a939431b9
peterfoeng <peter@peterfoeng.com>
2021-01-12 10:11:18 +1100

    add httpclientmodule to enable http service

--- gosports/Web/src/app/app.module.ts
+++ gosports/Web/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module';
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         AngularMaterialModule,
