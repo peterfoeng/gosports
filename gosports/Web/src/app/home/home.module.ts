01ad525c7816a90d63c04fc330d346ebfdba9718
peterfoeng <peter@peterfoeng.com>
2021-01-12 16:24:53 +1100

    adjust layout a bit

--- gosports/Web/src/app/home/home.module.ts
+++ gosports/Web/src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { routing } from './home.routing';
+import {AngularMaterialModule} from "../angular-material.module";
 
 @NgModule({
     imports: [
         CommonModule,
+        AngularMaterialModule,
         routing,
     ],
     declarations: [
