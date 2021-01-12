773afebec6c17994c85c26f9a49cc28f85927db2
peterfoeng <peter@peterfoeng.com>
2021-01-12 23:10:53 +1100

    unsubscribe api call

--- gosports/Web/src/app/matches/matches.component.ts
+++ gosports/Web/src/app/matches/matches.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IMatch } from './match.model';
 import { MatchService } from './match.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-matches',
     templateUrl: './matches.component.html',
     styleUrls: [ './matches.component.css' ]
 })
-export class MatchesComponent implements OnInit {
+export class MatchesComponent implements OnInit, OnDestroy {
     public data: IMatch[];
     public sportId: string;
+    public sub: Subscription;
 
     constructor(
         private activatedRoute: ActivatedRoute,
         private router: Router,
         private matchService: MatchService
-    ) {
-    }
+    ) { }
 
     ngOnInit(): void {
         this.sportId = this.activatedRoute.snapshot.params.id;
-        this.matchService.getMatches(this.sportId).subscribe(r => {
+        this.sub = this.matchService.getMatches(this.sportId).subscribe(r => {
             this.data = r;
         });
+    }
+
+    ngOnDestroy() {
+        this.sub?.unsubscribe();
     }
 
     redirect(id: string) {
--- gosports/Web/src/app/sports/sports.component.ts
+++ gosports/Web/src/app/sports/sports.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SportsService } from './sports.service';
-import {ISports} from "./sports.model";
+import { ISports } from './sports.model';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-sports',
     templateUrl: './sports.component.html',
     styleUrls: [ './sports.component.css' ]
 })
-export class SportsComponent implements OnInit {
+export class SportsComponent implements OnInit, OnDestroy {
     public data: ISports;
+    public sub: Subscription;
 
     constructor(
         private route: ActivatedRoute,
@@ -19,9 +21,13 @@ export class SportsComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.sportsService.getSports().subscribe(r => {
+        this.sub = this.sportsService.getSports().subscribe(r => {
             this.data = r;
         });
+    }
+
+    ngOnDestroy() {
+        this.sub?.unsubscribe();
     }
 
     redirect(sport: string): void {
