7542f207c2332525c05a89c8d5e7215757ffcefa
peterfoeng <peter@peterfoeng.com>
2021-01-12 22:58:50 +1100

    cleanup and update design

--- gosports/Web/src/app/core/app-header/app-header.component.ts
+++ gosports/Web/src/app/core/app-header/app-header.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './app-header.component.html',
     styleUrls: [ './app-header.component.scss' ]
 })
-export class AppHeaderComponent{
-
+export class AppHeaderComponent {
+    constructor(
+        private router: Router
+    ) { }
 }
